a0e1a7f0deb88b859ff4be53f5f646129a8e36c4
JacobMGEvans <jacobmgevans@gmail.com>
2021-09-05 23:49:12 -0500

    Worker Parsing - Gets GitHub content - Maps through DL URLS - Fetches file content Co-authored-by: name <skyfriends1@gmail.com>

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
 const { Octokit } = require('@octokit/rest');
-const got = require('got');
-
-addEventListener('fetch', event => {
-  event.passThroughOnException();
-  const { request } = event
-  return event.respondWith(handleRequest(request))
-})
-
-async function handleRequest(request) {
 
 const octokit = new Octokit({
   auth: GH_AUTH,
 });
 
-  // NOTE: Step 1 will be handled by the Guild Scribe
-  // Test locally HTTPIE hit the Worker endpoint - This will simulate the POST from Guild Scribe
-  // Get the  {owner: string; repo: string;} & First Issue information from POST coming from Guild Scribe
-  // 1. Use Object to get GH Repo data 
-  
-  // The following steps are handled by the worker:
-  //*  2. Use First Issue Config to know what data to parse (file types) (other configs Raid Leader, Repo Name, Raid Name)
-  //*  3. Take parsed repo from GH Octokit (content URL to Array [{RawUrl}]: DirectoriesInRepo RawURL gives the file content) 
-  //*  4. Utilize the file content and file name to generate a issue using the GH Octokit 
+addEventListener('fetch', event => {
+  const { request } = event
+  return event.respondWith(handleRequest(request))
+})
 
+async function handleRequest(request) {
+// Handled by the Guild Scribe: Get the  {owner: string; repo: string;} & First Issue information from POST coming from Guild Scribe
+
+// The following steps are handled by the worker:
+//*  TODO: 
+//*   1. Use First Issue Config to know what data to parse (file types) (other configs Raid Leader, Repo Name, Raid Name)
+//*  2. Take parsed repo from GH Octokit (content URL to Array [{RawUrl}]: DirectoriesInRepo RawURL gives the file content) 
 // Get the contents URL for the passed repo
-const rawResponse = await octokit.repos.getContent({ owner: 'OpenSourceRaidGuild', repo: 'TEST', path: '*' }) 
-const { contents_url: contentsUrl } = rawResponse.data
+const rawResponse = await octokit.repos.getContent({ owner: 'OpenSourceRaidGuild', repo: 'TEST', path: '' }) 
+const { data: allRefs } = rawResponse;
 
-console.log('URL for the repo contents: ', contentsUrl);
-const { data } = await got(contentsUrl)
-const contentUrls = rawContentData.json().map(file => file.download_url);
+// Get the download URLS for each file at that path
+const downloadUrls = allRefs.map(ref => ref.download_url);
 
-//! Currently not seeing the data in "request" when hitting the Worker endpoint with the POST body HTTPIE
+// Get the contents of those files
+let rawFileContents = await fetch(downloadUrls[1]);
+let fileContents = rawFileContents.body;
 
 
+return new Response(fileContents, {
+  headers: { 'Content-Type': 'application/json' },
+})
 
-    return new Response(contentsUrl, {
-      headers: { 'Content-Type': 'application/json' },
-    })
-
-  // raidsWebhook.on('issue.opened', testHook);
+  // TODO:
+  //*  3. Utilize the file content and file name to generate a issue using the GH Octokit
+  // octokit.issues.create()
+  
 }
